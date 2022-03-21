cb888156e097c6ca16810119ed16243236b04e0d
Rodrigo Andrade <59545857+DigoCAndrade@users.noreply.github.com>
2022-03-21 19:46:18 -0300

    Create Exercise_3.c

---
+++ AF2/Exercise_3.c
@@ -0,0 +1,12 @@
+// Exercício 3
+// Feito por Rodrigo Andrade
+
+#include <stdio.h>
+
+double resultado;
+
+int main() {
+    resultado = (5 + 8 + 12) / 3;
+    printf("A média aritimética entre os números 5, 8 e 12 é: %1.2f", resultado);
+    return 0;
+}
