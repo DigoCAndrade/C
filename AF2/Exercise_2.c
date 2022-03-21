28d6778db7910e653aa7ac2500adfb030a421091
Rodrigo Andrade <59545857+DigoCAndrade@users.noreply.github.com>
2022-03-21 19:45:31 -0300

    Create Exercise_2.c

---
+++ AF2/Exercise_2.c
@@ -0,0 +1,12 @@
+// Exercício 2
+// Feito por Rodrigo Andrade
+
+#include <stdio.h>
+
+double resultado;
+
+int main() {
+    resultado = (30 / 27);
+    printf("O resultado do produto entre os valores 30 e 27 é de: %1.2f", resultado);
+    return 0;
+}
