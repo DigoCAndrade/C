bf63b37f06f012be68963eda27fad9327cbba416
Rodrigo Andrade <59545857+DigoCAndrade@users.noreply.github.com>
2022-03-21 19:47:16 -0300

    Create Exercise_4.c

---
+++ AF2/Exercise_4.c
@@ -0,0 +1,13 @@
+// Exercício 4
+// Feito por Rodrigo Andrade
+
+#include <stdio.h>
+
+int numero;
+
+int main() {
+  printf("Digite um número inteiro:");
+  scanf("%d", &numero);
+  printf("O número que você digitou foi: %d", numero);
+  return 0;
+}
